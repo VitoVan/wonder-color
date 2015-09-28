799dbb524ab8ceed58be43a8a5730d249159edfc
Vito Van <awesomevito@live.com>
2015-09-28 18:24:35 +0800

    add cache

--- server.lisp
+++ server.lisp
@@ -24,19 +24,22 @@
 
 (defun controller-wonder-pic ()
   (let* ((pic-name (cl-ppcre:regex-replace-all " " (parameter "text") "-"))
-         (pic-url (search-pic (parameter "text"))))
-    (let ((file (open (concatenate 'string "pics/" pic-name)
-                      :direction :output
-                      :if-exists :rename-and-delete
-                      :element-type '(unsigned-byte 8)
-                      :if-does-not-exist :create))
-          (input (drakma:http-request pic-url
-                                      :want-stream t
-                                      :user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0")))
-      (when input
-        (loop for byte = (read-byte input nil nil)
-           while byte do (write-byte byte file))
-        (progn (close input) (close file))))
+         (pic-url (search-pic (parameter "text")))
+         (pic-file-path (concatenate 'string "pics/" pic-name)))
+    (or
+     (fad:file-exists-p pic-file-path)
+     (let ((file (open pic-file-path
+                       :direction :output
+                       :if-exists :rename-and-delete
+                       :element-type '(unsigned-byte 8)
+                       :if-does-not-exist :create))
+           (input (drakma:http-request pic-url
+                                       :want-stream t
+                                       :user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0")))
+       (when input
+         (loop for byte = (read-byte input nil nil)
+            while byte do (write-byte byte file))
+         (progn (close input) (close file)))))
     pic-name))
 
 (setf *dispatch-table*
