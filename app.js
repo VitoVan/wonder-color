e0dfab882e6310a9a1ad765bc096c80b5bd22cc3
Vito Van <awesomevito@live.com>
2015-09-29 08:37:24 +0800

    remove console log & add color hex

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function wonder(text,url){
     $('.color-block').remove();
     $('#cssload-pgloading').show();
     $.post('/wonder',{text: text},function(data){
-        console.log("/pics/" + data);
         var img = document.createElement('img');
         img.setAttribute('src',"/pics/" + data);
         img.addEventListener('load', function() {
@@ -11,8 +10,11 @@ function wonder(text,url){
             var rendered = false;
             for (swatch in swatches)
                 if (swatches.hasOwnProperty(swatch) && swatches[swatch]){
-                    console.log(swatch, swatches[swatch].getHex());
+                    var invertColor = 'rgb(' + (255 - swatches[swatch].getRgb()[0]) + ','
+                        + (255 - swatches[swatch].getRgb()[1]) + ',' + (255 - swatches[swatch].getRgb()[2]) + ')';
                     $('<div>')
+                        .css('color',invertColor)
+                        .text(swatches[swatch].getHex().toUpperCase())
                         .addClass('color-block')
                         .css('backgroundColor',swatches[swatch].getHex())
                         .insertAfter($('#colorname'));
